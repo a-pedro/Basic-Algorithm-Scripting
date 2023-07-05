e0703466a4210b2e140b43982e19c0d33cc2438d
a-pedro <116759801+a-pedro@users.noreply.github.com>
2023-07-05 16:59:04 +0200

    Add files via upload

---
+++ chunky-monkey.js
@@ -0,0 +1,11 @@
+function chunkArrayInGroups(arr, size) {
+  const newArr = [];
+  let i = 0;
+  while (i < arr.length){
+    newArr.push(arr.slice(i, i + size));
+    i += size;
+  }
+  return newArr;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
